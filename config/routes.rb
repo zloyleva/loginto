7104a717c889aed24500578cbd02e0318aac71b3
zloyleva <zloyleva@gmail.com>
2016-10-30 21:20:35 +0200

    Add callback routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   get 'home/index'
   root 'home#index'
 
+  match "/auth/:provider/callback" => "sessions#create", :via => [:get, :post]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
