e70a78d292570f8d0fa671f96fe913912b04132a
zloyleva <zloyleva@gmail.com>
2016-10-30 21:42:00 +0200

    Add destroy routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root 'home#index'
 
   match "/auth/:provider/callback" => "sessions#create", :via => [:get, :post]
-
+  match "/signout" => "sessions#destroy", :as => :signout, :via => [:delete]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
