279f6740d7e0251ef35fbbadf09db5a8c78836c2
zloyleva <zloyleva@gmail.com>
2016-10-30 23:31:22 +0200

    Redirect if user authorization

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,11 @@
 class HomeController < ApplicationController
 
   def index
+    redirect_to home_profile_url if session[:user_id]
   end
 
   def profile
+    redirect_to home_index_url if !session[:user_id]
   end
 
 end
